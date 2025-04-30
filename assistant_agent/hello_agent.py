164cd0dfc655d81cb767dd3f7a434ba5da3fd519
Emmanuel Amador Maldonado <128644954+EmmanuelMald@users.noreply.github.com>
2025-04-30 12:21:28 -0600

    Agent Tool: Image Prompt Generation (#2)
    * Tool to create a prompt to be used in a image generator app based on a basic idea
    
    * Modify agent to use its first tool, the image prompt generator

--- assistant_agent/hello_agent.py
+++ assistant_agent/hello_agent.py
@@ -1,20 +1,34 @@
 from loguru import logger
-from pydantic_ai import Agent
+from pydantic_ai import Agent, Tool
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
 import sys
 
 sys.path.append("..")
+
 from assistant_agent.credentials import get_llm_config
+from assistant_agent.tools.prompt_image_generator import generate_prompt_image
 
 llm_config = get_llm_config()
+
+system_prompt = "You are a specialized AI assistant, an expert prompt engineer"
 
 model = GeminiModel(
     llm_config.MODEL,
     provider=GoogleGLAProvider(api_key=llm_config.API_KEY.get_secret_value()),
 )
 
-agent = Agent(model)
+agent = Agent(
+    model,
+    tools=[
+        Tool(generate_prompt_image, takes_ctx=False),
+    ],
+    system_prompt=system_prompt,
+)
 
-result = agent.run_sync("Where does 'hello world' come from?")
+
+result = agent.run_sync(
+    "Help me generate a prompt to generate an image"
+    "to be printed in a t-shirt. The concept is 'quantum entanglement'."
+)
 logger.info(f"{result.output}")
