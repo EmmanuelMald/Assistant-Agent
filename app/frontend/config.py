2408de47e643a00659c0921bf746f54e3f0c0b95
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-06 16:11:17 -0600

    Use the URL of the Agent API deployed

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -2,5 +2,5 @@ from pydantic_settings import BaseSettings
 
 
 class BackendInfo(BaseSettings):
-    BASE_URL: str = "http://localhost:8000"
+    BASE_URL: str = "https://agent-api-214571216460.northamerica-south1.run.app"
     AGENT_REQUEST_ENDPOINT: str = "/ask_agent"
