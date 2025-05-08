863cd1c7336ec52843881f4a4baabd76d6be34ef
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-08 15:57:36 -0600

    Add a field validator fo the hashed_password to change None values to empty strings to avoid error using get_secret_value()

--- app/backend/models.py
+++ app/backend/models.py
@@ -70,3 +70,10 @@ class UserInDB(BaseModel):
     hashed_password: Optional[SecretStr] = Field(
         description="Hashed password stored in DB"
     )
+
+    @field_validator("hashed_password", mode="after")
+    @classmethod
+    def validate_hashed_password(cls, value):
+        if value is None:
+            return ""
+        return value
