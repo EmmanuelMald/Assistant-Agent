c99cebc404e30086ecbdde0667e6ef4ea8e76c30
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-08 14:35:45 -0600

    Modify User model

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field, EmailStr, field_validator
 from typing import Optional
 import sys
 
@@ -19,12 +19,38 @@ class AgentResponse(BaseModel):
 
 
 class User(BaseModel):
-    full_name: str = Field(description="Full name of the user")
+    full_name: str = Field(
+        description="Full name of the user",
+        min_length=1,
+        pattern=r"^[^\s].*",  # To not start with a space
+    )
     company_name: Optional[str] = Field(
-        default=None, description="Name of the company where the user works for"
+        default=None,
+        description="Name of the company where the user works for",
+        pattern=r"^[^\s].*",  # To not start with a space
     )
     email: EmailStr = Field(description="User's email")
     company_role: Optional[str] = Field(
         default=None,
         description="Role that the user has in the company where he's working on",
+        pattern=r"^[^\s].*",  # To not start with a space
     )
+
+    @field_validator("full_name", mode="after")
+    @classmethod
+    def normalize_full_name(cls, value):
+        return value.strip().title()
+
+    @field_validator("company_name", mode="after")
+    @classmethod
+    def normalize_company_name(cls, value):
+        if value not in [None, ""]:
+            return value.strip().title()
+        return None
+
+    @field_validator("company_role", mode="after")
+    @classmethod
+    def normalize_company_role(cls, value):
+        if value not in [None, ""]:
+            return value.strip().title()
+        return None
