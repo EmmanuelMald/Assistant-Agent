e407deb6aaa2adddcf61a2e092f830258e27abf7
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-08 14:03:26 -0600

    Creation of a User pydantic model

--- app/backend/models.py
+++ app/backend/models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 import sys
 
@@ -16,3 +16,15 @@ class AgentRequest(BaseModel):
 class AgentResponse(BaseModel):
     agent_response: str = Field(description="Agent response")
     current_history: str = Field(description="Whole chat session history")
+
+
+class User(BaseModel):
+    full_name: str = Field(description="Full name of the user")
+    company_name: Optional[str] = Field(
+        default=None, description="Name of the company where the user works for"
+    )
+    email: EmailStr = Field(description="User's email")
+    company_role: Optional[str] = Field(
+        default=None,
+        description="Role that the user has in the company where he's working on",
+    )
