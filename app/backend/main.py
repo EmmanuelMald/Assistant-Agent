d37c3f571be9d763f5134caabb6a9d95784cc902
Emmanuel Amador Maldonado <128644954+EmmanuelMald@users.noreply.github.com>
2025-05-08 22:16:20 -0600

    Create an endpoint to create users (#39)
    * Create a UserRegistrationResponse model for the API
    
    * Create an endpoint to register a user
    
    * Add /add_user endpoint to the BackendInfo config class
    
    * Modification of the location value in the /add_user endpoint
    
    * Add test for the /add_user endpoint

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, status, Response
 from loguru import logger
 
 import sys
 
 sys.path.append("../..")
 
-from app.backend.models import AgentRequest, AgentResponse
+from app.backend.models import AgentRequest, AgentResponse, UserRegistrationResponse
+from assistant_agent.schemas import User
 from assistant_agent.agent import generate_agent_instance
+from assistant_agent.database import register_new_user
 from assistant_agent.auxiliars.agent_auxiliars import (
     prepare_to_read_chat_history,
     prepare_to_send_chat_history,
@@ -32,7 +34,9 @@ async def agent_request(request: AgentRequest):
 
     except Exception as e:
         logger.error(e)
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
 
     new_chat_history_binary = agent_answer.all_messages_json()
     new_chat_history = prepare_to_send_chat_history(new_chat_history_binary)
@@ -41,3 +45,37 @@ async def agent_request(request: AgentRequest):
     )
 
     return response
+
+
+@app.post(
+    "/add_user",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserRegistrationResponse,
+)
+def add_user(user_data: User, response: Response):
+    try:
+        user_id = register_new_user(user_data=user_data)
+        response.headers["Location"] = f"/users/{user_id}"
+    except ValueError as ve:
+        if "email is already registered" in str(ve).lower():
+            logger.warning(f"Conflict during user registration: {ve}")
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(ve))
+        else:
+            logger.error(f"Invalid user data provided: {ve}")
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=f"Invalid data: {str(ve)}",
+            )
+    except Exception as e:
+        # Any other exception
+        logger.error(
+            f"Unexpected error during user registration: {e}", exc_info=True
+        )  # exc_info=True for traceback
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An internal server error occurred while registering the user.",
+        )
+
+    message = "User registered successfully!"
+
+    return UserRegistrationResponse(user_id=user_id, message=message)
