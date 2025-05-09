0e49cb54593367b2526f1360f84c150051b3ba99
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-08 21:46:03 -0600

    Add /add_user endpoint to the BackendInfo config class

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -4,3 +4,4 @@ from pydantic_settings import BaseSettings
 class BackendInfo(BaseSettings):
     BASE_URL: str = "https://agent-api-214571216460.northamerica-south1.run.app"
     AGENT_REQUEST_ENDPOINT: str = "/ask_agent"
+    CREATE_USER_ENDPOINT: str = "/add_user"
