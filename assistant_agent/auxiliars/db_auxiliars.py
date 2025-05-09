c5b099c917656aec4a16c2e3e4e95c8bea31fef1
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-08 20:04:05 -0600

    Add insert_user_data to db_auxiliars

--- assistant_agent/auxiliars/db_auxiliars.py
+++ assistant_agent/auxiliars/db_auxiliars.py
@@ -1,12 +1,13 @@
 from loguru import logger
+from datetime import datetime
 from pydantic import SecretStr
 
 import sys
 
 sys.path.append("../..")
 
-from app.backend.models import UserInDB
-from assistant_agent.utils.gcp.bigquery import query_data
+from app.backend.models import UserInDB, User
+from assistant_agent.utils.gcp.bigquery import query_data, insert_rows
 from assistant_agent.config import GCPConfig
 
 
@@ -72,4 +73,51 @@ def generate_user_id(table_id: str = gcp_config.USERS_TABLE_NAME) -> str:
     next_id = total_users + 1
     user_id = f"UID{next_id:05d}"
 
+    return user_id
+
+
+def insert_user_data(user_data: User, table_id=gcp_config.USERS_TABLE_NAME) -> str:
+    """
+    Insert user data into the BigQuery database.
+
+    Args:
+        user_data: User -> User class containing the user information
+        table_id (str): The name of the BigQuery table.
+
+    Returns:
+        str -> user_id that was inserted into the BigQuery table.
+    """
+    logger.info("Inserting user data into BigQuery...")
+
+    # Get the current date and time
+    now = datetime.now()
+    current_time = now.strftime(r"%Y-%m-%d %H:%M:%S")
+
+    logger.info("Generating a new user ID...")
+    user_id = generate_user_id(table_id=table_id)
+    logger.info(f"Generated user ID: {user_id}")
+
+    logger.info("Inserting data...")
+    # Preparing the columns to fill in the BigQuery table
+    data_to_insert = {
+        "user_id": user_id,
+        "full_name": user_data.full_name,
+        "company_name": user_data.company_name,
+        "email": user_data.email,
+        "company_role": user_data.company_role,
+        "created_at": current_time,
+        "last_entered_at": current_time,
+        "hashed_password": user_data.password,  # Supposing that password is already hashed
+    }
+
+    # Insert the data into the BigQuery table
+    insert_rows(
+        project_id=project_id,
+        dataset_name=dataset_id,
+        table_name=table_id,
+        rows=[
+            data_to_insert,
+        ],
+    )
+    logger.info("user data successfully added to the database")
     return user_id
