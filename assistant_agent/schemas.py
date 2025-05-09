68a384f5f957a04174e6c48c57a862af22b53c4e
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-09 15:54:53 -0600

    ChatSession schema created

--- assistant_agent/schemas.py
+++ assistant_agent/schemas.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr, field_validator, SecretStr
+from pydantic import (
+    BaseModel,
+    Field,
+    EmailStr,
+    field_validator,
+    SecretStr,
+    model_validator,
+)
 from typing import Optional
+from datetime import datetime
 
 
 class User(BaseModel, validate_assignment=True):
@@ -48,6 +56,56 @@ class User(BaseModel, validate_assignment=True):
         if value not in [None, ""]:
             return value.strip().title()
         return None
+
+
+class ChatSession(BaseModel, validate_assignment=True):
+    user_id: str = Field(
+        description="Id of the owner of the session", pattern=r"^UID\d{5}$"
+    )
+    chat_session_id: Optional[str] = Field(
+        default=None, description="ID of the chat session", pattern=r"CSID\d+-\d{3}$"
+    )
+    created_at: Optional[str] = Field(
+        default=None,
+        description="Date where the chat session was created. In format YYYY-MM-DD",
+        pattern=r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$",
+    )
+    last_used_at: Optional[str] = Field(
+        default=None,
+        description="Last date when the session was used. In format YYYY-MM-DD",
+        pattern=r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$",
+    )
+    session_history: Optional[str] = Field(
+        default="[]", description="Full chat session history"
+    )
+
+    @field_validator("session_history", mode="after")
+    @classmethod
+    def session_history_handle_nulls(cls, value):
+        if value is None:
+            return "[]"
+        return value
+
+    @model_validator(mode="after")
+    def validate_dates(self):
+        if self.created_at is not None and self.last_used_at is None:
+            self.last_used_at = self.created_at
+
+        elif self.last_used_at is not None and self.created_at is None:
+            raise ValueError("Missing created_at")
+
+        elif self.created_at is not None and self.last_used_at is not None:
+            created_at_datetime = datetime.strptime(
+                self.created_at, r"%Y-%m-%d %H:%M:%S"
+            )
+            last_used_at_datetime = datetime.strptime(
+                self.last_used_at, r"%Y-%m-%d %H:%M:%S"
+            )
+
+            if created_at_datetime > last_used_at_datetime:
+                raise ValueError("created_at cannot be greater than last_used_at")
+
+        return self
 
 
 class UserInDB(BaseModel):
