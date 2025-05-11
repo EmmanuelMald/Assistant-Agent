cc5aaa79cb87c3de41e705b66cff5a9b11417235
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-11 04:14:18 -0600

    Create a BQAgentStepsTable with the _generate_id method

--- assistant_agent/database/tables/bigquery/agent_history.py
+++ assistant_agent/database/tables/bigquery/agent_history.py
@@ -1,16 +1,21 @@
-from assistant_agent.database.tables.bigquery import BigQueryTable, BQPromptsTable
+from .bq_base import BigQueryTable
+from assistant_agent.database.tables.bigquery import BQPromptsTable, BQChatSessionsTable
 from assistant_agent.config import GCPConfig
+from assistant_agent.utils.gcp.bigquery import query_data
+from loguru import logger
+import re
 
 gcp_config = GCPConfig()
 
 
-class AgentHistory(BigQueryTable):
+class BQAgentStepsTable(BigQueryTable):
     __name: str = gcp_config.AGENT_STEPS_TABLE_NAME
     __primary_key: str = gcp_config.AGENT_STEPS_TABLE_PK
 
     def __init__(self):
         super().__init__()
         self._prompts_table: BQPromptsTable = BQPromptsTable()
+        self._sessions_table: BQChatSessionsTable = BQChatSessionsTable()
 
     @property
     def name(self):
@@ -19,3 +24,44 @@ class AgentHistory(BigQueryTable):
     @property
     def primary_key(self):
         return self.__primary_key
+
+    def _generate_id(self, prompt_id: str, chat_session_id: str) -> str:
+        """
+        Generate an agent_step_id
+
+        Args:
+            prompt_id: str -> ID of the prompt that generated the step
+            chat_session_id: str -> ID of the chat session where the prompt_id belongs to
+
+        Returns:
+            str -> agent_step_id
+        """
+        if not self._prompts_table.prompt_exists(prompt_id):
+            raise ValueError("The prompt_id does not exist")
+
+        if not self._sessions_table.session_exists(chat_session_id):
+            raise ValueError("The chat_session_id does not exist")
+
+        query = f"""
+                select
+                    count(*) as total_session_steps
+                from {self.project_id}.{self.dataset_id}.{self.name}
+                where chat_session_id = '{chat_session_id}'
+                """
+
+        # Query the BigQuery database to get the total number of sessions of the user
+        row_iterator = query_data(query=query)
+
+        session_steps = next(row_iterator).total_session_steps
+
+        # Generate the step_id
+        next_id = session_steps + 1
+
+        # Extracting the prompt number from the prompt_id to generate a step_id
+        match = re.search(r"\d+", prompt_id)
+        prompt_number = int(match.group(0))
+
+        step_id = f"AST{prompt_number}-{next_id:08d}"
+        logger.info(f"{step_id = }")
+
+        return step_id
