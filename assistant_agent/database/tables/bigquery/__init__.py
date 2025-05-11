8b568e3ba53ee66a27fd14394768ea2674bca936
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-11 02:57:50 -0600

    Refactor: Simplify BigQuery table class imports

---
+++ assistant_agent/database/tables/bigquery/__init__.py
@@ -0,0 +1,9 @@
+from .users import BQUsersTable
+from .chat_sessions import BQChatSessionsTable
+from .prompts import BQPromptsTable
+
+__all__ = [
+    "BQUsersTable",
+    "BQChatSessionsTable",
+    "BQPromptsTable",
+]
