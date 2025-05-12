d68034271234754f6545539eb9765eeda263ca22
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-11 22:55:02 -0600

    Fix create_access_token function

--- assistant_agent/authentication.py
+++ assistant_agent/authentication.py
@@ -6,6 +6,7 @@ from assistant_agent.auxiliars.auth_auxiliars import verify_password
 from assistant_agent.schemas import UserInDB
 from assistant_agent.credentials import get_auth_config
 from assistant_agent.database.tables.bigquery import BQUsersTable
+from loguru import logger
 
 
 auth_config = get_auth_config()
@@ -34,16 +35,18 @@ def authenticate_user(email: EmailStr, password: SecretStr) -> Optional[UserInDB
     return user_data
 
 
-def create_acess_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     """
     Creates an access token for the user
 
     Args:
-        data: dict -> Must be in the format {"sub": user_identifier}
+        data: dict -> Must include the data that you want to be included inside the token
+                      By convention, one parameter should be {'sub': subject_id }
         expires_delta: Optional[timedelta] -> token lifetime limit
 
     """
     to_encode = data.copy()
+    logger.info("Creating access token...")
 
     # Set the expiration time
     if expires_delta:
@@ -53,10 +56,15 @@ def create_acess_token(data: dict, expires_delta: Optional[timedelta] = None) ->
             minutes=auth_config.ACCESS_TOKEN_EXPIRE_MINUTES
         )
 
+    # exp = expiration time in JWT
     to_encode.update({"exp": expire})
+    logger.info(f"Token expires at {expire}")
 
     encoded_jwt = jwt.encode(
-        to_encode, auth_config.SECRET_KEY, algorithm=auth_config.ALGORITHM
+        to_encode,
+        auth_config.SECRET_KEY.get_secret_value(),
+        algorithm=auth_config.ALGORITHM,
     )
+    logger.info("Access token successfully created")
 
     return encoded_jwt
