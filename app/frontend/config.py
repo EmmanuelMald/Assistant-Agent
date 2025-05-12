6780f63bc848e77ee8baef369854c9af8b85dab0
Emmanuel Amador <emmanuel_mald@hotmail.com>
2025-05-12 17:33:05 -0600

    Make the login page the main one

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -3,5 +3,5 @@ from pydantic_settings import BaseSettings
 
 class PagesConfig(BaseSettings):
     chat_agent: str = "pages/chat_agent.py"
-    login: str = "pages/login.py"
-    registration: str = "registration_page.py"
+    login: str = "login.py"
+    registration: str = "pages/registration_page.py"
